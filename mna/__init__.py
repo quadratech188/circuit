45220d039fdf5571db1877f0b58a804d9fb743f1
Quadratech188 <quadratech188@gmail.com>
2025-05-27 22:14:51 +0900

    feat: Import classes directly in __init__.py

---
+++ mna/__init__.py
@@ -0,0 +1,5 @@
+from .Element import Element
+from .Simulation import Simulation
+from .StateBuilder import StateBuilder
+from .Diode import Diode
+from .Common import VoltageSource, CurrentSource, Resistor, Capacitor, Inductor
